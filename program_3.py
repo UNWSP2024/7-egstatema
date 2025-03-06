992af288fb707b7b241a44d78a7d766743cde2bf
egstatema <egstatema@students.unwsp.edu>
2025-03-06 16:57:26 -0600

    Add files via upload

--- program_3.py
+++ program_3.py
@@ -1,25 +1,29 @@
-# Program #3: US_Population
+# Eliya Statema
+# 3/6/25
+# Population
+
 def main():
-    # Have the user input (using a loop) various information that contains three pieces of data: 
-    # year, name of state, and population.  
-    # Store all of this information in a list of lists.  For example it might be stored like this:
-    
-    # [[2010, "Maine", 1987435], [2010,"Minnesota",6873202], [2011, "Iowa", 3421988]]
-    all_entered_values = []
+    data = []
+    while True:
+        add_more = input("Add state data (y/n)? ")
+        if add_more == "y":
+            year = int(input("Enter the year: "))
+            state = input("Enter the state name: ")
+            population = int(input("Enter the population: "))
+            data.append([year, state, population])
+        else:
+            break
 
-    # Now have the user enter a year. 
-    
-    # The program will add the populations from all states in the list of list for that year only.
-    # Pass the list and year to the sum_population_for_year
+    target_year = int(input("Enter the year to calculate population for: "))
+    total_population = sum_population_for_year(data, target_year)
+    print(f"The total population for the year {target_year} is {total_population}.")
 
-def sum_population_for_year(all_entered_values, year_to_sum):
-    # Loop through and sum the populations for the appropriate year. 
-    # e.g. for the list on line 7 the total would be 8,860,637 if the user enterd 2010 for the year to sum,
-    # or 3,421,988 if they enterd 2011 for the year to sum.
+def sum_population_for_year(data, target_year):
+    total_population = 0
+    for year in data:
+        if year[0] == target_year:
+             total_population += year[2]
+    return total_population
 
-    # print the totalled population
-
-
-# Call the main function.
 if __name__ == '__main__':
     main()
--- program_4.py
+++ program_4.py
@@ -1,10 +1,26 @@
-# Program #4: Coordinates
-# Write a distance function that will take two 3-dimensional coordinates (as input) 
-# and will return (as output) the distance between those points in space.  
-# The 3-dimensional coordinates must be stored as tuples.
+# Eliya Statema
+# 3/6/25
+# Coordinates
 
-# Now write a mainline that has the user enter the two tuples.  
-# The mainline calls the distance function and stores the distance in a variable.  The mainline then displays the distance.  
-# Also include exception handling to deal with faulty input.
-# The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
-#    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+import math
+
+def get_coordinates(coord_1, coord_2):
+    return math.sqrt((coord_2[0] - coord_1[0]) ** 2 +
+                     (coord_2[1] - coord_1[1]) ** 2 +
+                     (coord_2[2] - coord_1[2]) ** 2)
+
+def main():
+    try:
+        x1, y1, z1 = map(float, input("Enter the first coordinates (x, y, z): ").split(","))
+        coord_1 = (x1, y1, z1)
+
+        x2, y2, z2 = map(float, input("Enter the second coordinates (x, y, z): ").split(","))
+        coord_2 = (x2, y2, z2)
+    except ValueError:
+        print("Invalid input. Please enter numbers separated by commas.")
+
+    distance = get_coordinates(coord_1, coord_2)
+    print(f"The distance between these points is: {distance:.2f}")
+
+if __name__ == "__main__":
+    main()
